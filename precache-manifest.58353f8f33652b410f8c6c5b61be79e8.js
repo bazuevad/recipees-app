ce146547ddfbee5f13100380d41dd90ad479cde2
Daria Bazueva <daria@Darias-MBP.fios-router.home>
2020-06-30 01:31:00 -0400

    Updates

---
+++ precache-manifest.58353f8f33652b410f8c6c5b61be79e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad287592bb3798b05952e597aff19512",
+    "url": "/recipees-app/index.html"
+  },
+  {
+    "revision": "fbc4927a1b6990e9319c",
+    "url": "/recipees-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9b3cd226e0b5623723a",
+    "url": "/recipees-app/static/js/2.576a4c1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipees-app/static/js/2.576a4c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc4927a1b6990e9319c",
+    "url": "/recipees-app/static/js/main.9459a09b.chunk.js"
+  },
+  {
+    "revision": "5176dfe5734993f0751b",
+    "url": "/recipees-app/static/js/runtime-main.ddad8a9d.js"
+  }
+]);
