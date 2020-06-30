a6e514eaa852bb6963b5c24a8ece37167e736393
Daria Bazueva <daria@Darias-MBP.fios-router.home>
2020-06-30 16:13:33 -0400

    Make a change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useEffect,useState} from 'react';
 import './App.css';
+import Recipe from './Recipees' ;
 
 const App = () => {
   var APP_ID = '650b4ba9';
-  var APP_KEY = 'a9e7a75fa0dd44368a600a3b7d20888d';
-  var base_url = 'https://api.spoonacular.com/recipes/search?query=cheese&number=2&apiKey=a9e7a75fa0dd44368a600a3b7d20888d';
+  var APP_KEY = 'fe8e990ccf288435f9d7a0f88d403a7836';
+  var base_url = 'https://api.spoonacular.com/recipes/search?query=cheese&number=10&apiKey=a9e7a75fa0dd44368a600a3b7d20888d';
   
-  
+  const [recipees, setRecipees] = useState([]);
   useEffect(()=>{
     getRecipes();
   },[]);
@@ -14,7 +15,9 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(base_url);
     const data = await response.json();
-    console.log(data);
+    //console.log(data.results);
+    setRecipees(data.results);
+    console.log(data.results);
 
   }
   return (
@@ -23,6 +26,9 @@ const App = () => {
         <input className = "search-input" type="text"/>
         <button className = "search-button" type = "submit">Search</button>
       </form>
+      {recipees.map(recipe =>(
+        <Recipe  />
+      ))};
     </div>
   );
 }
---
+++ src/Recipees.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Recipe = () => {
+    return(
+        <div>
+            <h1>Titles</h1>
+            <p>Calories</p>
+        </div>
+    );
+}
+
+export default Recipe;
